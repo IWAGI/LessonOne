0217f4d163fd96ac216f0fc0137488bec77a02d8
IWAGI <probel54@yandex.ru>
2022-02-07 20:32:08 +0700

    Add button and action.

--- LessonOne/ViewController.swift
+++ LessonOne/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.isHidden = true
+        
+        let topColor = UIColor(red: 219/255, green: 98/255, blue: 222/255, alpha: 1)
+        let bottomColor = UIColor(red: 174/255, green: 81/255, blue: 222/255, alpha: 1)
+        
+        let gradientColors = [topColor.cgColor, bottomColor.cgColor]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        
+        gradientLayer.frame = button.bounds
+        
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if label.isHidden {
+            button.setTitle("Hide text", for: .normal)
+            label.isHidden = false
+        } else {
+            button.setTitle("Show text", for: .normal)
+            label.isHidden = true
+        }
+    }
+    
 }
 
