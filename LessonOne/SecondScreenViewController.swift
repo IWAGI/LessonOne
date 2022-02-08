a53fd45f2d0c5bcf040e0758ab8d42bffb00c5d3
IWAGI <probel54@yandex.ru>
2022-02-08 15:03:03 +0700

    Added closing second screens on button click.

--- LessonOne/SecondScreenViewController.swift
+++ LessonOne/SecondScreenViewController.swift
@@ -11,7 +11,9 @@ class SecondScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .cyan
+    }
+    
+    @IBAction func backButtonTapped() {
+        dismiss(animated: true)
     }
 }
